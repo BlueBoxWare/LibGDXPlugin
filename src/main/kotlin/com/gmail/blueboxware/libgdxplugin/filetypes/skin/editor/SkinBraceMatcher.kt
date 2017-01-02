ece0603803465ced1e9350625d425add99b00feb
BlueBoxWare <BlueBoxWare@users.noreply.github.com>
2017-01-02 17:27:02 +0100

    Brace matching for Skin files

---
+++ src/main/kotlin/com/gmail/blueboxware/libgdxplugin/filetypes/skin/editor/SkinBraceMatcher.kt
@@ -0,0 +1,34 @@
+package com.gmail.blueboxware.libgdxplugin.filetypes.skin.editor
+
+import com.gmail.blueboxware.libgdxplugin.filetypes.skin.SkinElementTypes
+import com.intellij.lang.BracePair
+import com.intellij.lang.PairedBraceMatcher
+import com.intellij.psi.PsiFile
+import com.intellij.psi.tree.IElementType
+
+/*
+ * Copyright 2017 Blue Box Ware
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+class SkinBraceMatcher: PairedBraceMatcher {
+
+  override fun getCodeConstructStart(file: PsiFile?, openingBraceOffset: Int) = openingBraceOffset
+
+  override fun getPairs() = arrayOf(
+          BracePair(SkinElementTypes.L_BRACKET, SkinElementTypes.R_BRACKET, true),
+          BracePair(SkinElementTypes.L_CURLY, SkinElementTypes.R_CURLY, true)
+  )
+
+  override fun isPairedBracesAllowedBeforeType(lbraceType: IElementType, contextType: IElementType?) = true
+}
