5c05c26a53999162dd027713946760d6258558b8
BlueBoxWare <BlueBoxWare@users.noreply.github.com>
2018-08-05 17:03:41 +0200

    TaggedClassImplicitUsageProvider: Don't mark classes used in Skins with tags as unused

---
+++ src/main/kotlin/com/gmail/blueboxware/libgdxplugin/filetypes/skin/references/TaggedClassImplicitUsageProvider.kt
@@ -0,0 +1,50 @@
+package com.gmail.blueboxware.libgdxplugin.filetypes.skin.references
+
+import com.intellij.codeInsight.daemon.ImplicitUsageProvider
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiElement
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.searches.ReferencesSearch
+
+
+/*
+ * Copyright 2018 Blue Box Ware
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+class TaggedClassImplicitUsageProvider: ImplicitUsageProvider {
+
+  override fun isImplicitUsage(element: PsiElement): Boolean {
+
+    if (element !is PsiClass) {
+      return false
+    }
+
+    val parameters =
+            ReferencesSearch.SearchParameters(element, GlobalSearchScope.allScope(element.project), true)
+
+    var found = false
+    TaggedClassUsagesSearcher().execute(parameters) { reference ->
+      found = true
+      return@execute false
+    }
+
+    return found
+
+  }
+
+  override fun isImplicitRead(element: PsiElement?): Boolean = false
+
+  override fun isImplicitWrite(element: PsiElement?): Boolean = false
+
+}
