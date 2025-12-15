c12753a8ace4d36e65f0c2c6cb2763333b76c0ca
BlueBoxWare <BlueBoxWare@users.noreply.github.com>
2025-12-15 07:29:12 +0100

    gdxAI Behavior Trees: brace matching and quote handling.

---
+++ src/main/kotlin/com/gmail/blueboxware/libgdxplugin/filetypes/tree/editor/TreeBraceMatcher.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2025 Blue Box Ware
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gmail.blueboxware.libgdxplugin.filetypes.tree.editor
+
+import com.gmail.blueboxware.libgdxplugin.filetypes.tree.TreeElementTypes
+import com.intellij.lang.BracePair
+import com.intellij.lang.PairedBraceMatcher
+import com.intellij.psi.PsiFile
+import com.intellij.psi.tree.IElementType
+
+class TreeBraceMatcher : PairedBraceMatcher {
+    override fun getPairs(): Array<out BracePair?> = PAIRS
+
+    override fun isPairedBracesAllowedBeforeType(
+        lbraceType: IElementType,
+        contextType: IElementType?
+    ): Boolean = true
+
+    override fun getCodeConstructStart(file: PsiFile?, openingBraceOffset: Int): Int = openingBraceOffset
+}
+
+private val PAIRS = arrayOf(BracePair(TreeElementTypes.LPAREN, TreeElementTypes.RPAREN, true))
